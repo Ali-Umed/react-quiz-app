b3fd7789c29b4e4bbec845d1e2ca412eab97bf1a
Ali-Umed <aliumed844@gmail.com>
2024-03-15 17:39:01 +0300

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <p className="read-the-docs">
+      <p className="text-red-900 ">
         Click on the Vite and React logos to learn more
       </p>
     </>

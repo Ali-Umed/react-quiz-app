8b46df01eb2575e6f644c7e17f07ae53da242102
Ali-Umed <aliumed844@gmail.com>
2024-03-16 17:03:15 +0300

    change  size container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
   return (
-    <>
-      <p className="text-red-900 ">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-gray-200">
+      <div className="flex justify-center items-center w-96 h-96 bg-teal-50">
+        <p className="text-white">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </div>
   );
 }
 

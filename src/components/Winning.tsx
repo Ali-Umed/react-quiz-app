3d928a877a722c9d8bbafd7d5f4e441d8e56e132
Ali-Umed <aliumed844@gmail.com>
2024-03-21 23:03:29 +0300

    change style to fire

--- src/components/Winning.tsx
+++ src/components/Winning.tsx
@@ -1,26 +1,42 @@
-import { useCallback } from "react";
-import type { Container, Engine } from "tsparticles-engine";
+import React, { useCallback, useEffect, useRef } from "react";
+import {
+  Container,
+  Engine,
+  Particles as ParticlesType,
+} from "tsparticles-engine";
 import Particles from "react-tsparticles";
-//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+import { loadSlim } from "tsparticles-slim";
 
-export const Winning = () => {
+export const Fireworks = () => {
+  const containerRef = useRef<Container>();
+  const particlesRef = useRef<ParticlesType>();
+
+  useEffect(() => {
+    if (containerRef.current && particlesRef.current) {
+      const interval = setInterval(() => {
+        const particleCount = Math.floor(Math.random() * 100) + 50; // Randomize particle count for each explosion
+        particlesRef.current.options.particles.number.value = particleCount;
+        particlesRef.current.refresh();
+      }, 2000); // Interval between explosions (in milliseconds)
+
+      return () => clearInterval(interval);
+    }
+  }, []);
+
   const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
     await loadSlim(engine);
   }, []);
 
   const particlesLoaded = useCallback(
     async (container: Container | undefined) => {
-      await console.log(container);
+      if (container) {
+        containerRef.current = container;
+        particlesRef.current = container.particles;
+      }
     },
     []
   );
+
   return (
     <Particles
       id="tsparticles"
@@ -29,68 +45,36 @@ export const Winning = () => {
       options={{
         background: {
           color: {
-            value: "#0d4101",
+            value: "#000000",
           },
         },
-        fpsLimit: 120,
+        fpsLimit: 60,
         interactivity: {
           events: {
             onClick: {
-              enable: true,
-              mode: "push",
+              enable: false,
             },
             onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
+              enable: false,
             },
           },
         },
         particles: {
           color: {
-            value: "#affaaa",
-          },
-          links: {
-            color: "#faafaa",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
+            value: "#ffffff",
           },
           move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 6,
-            straight: false,
+            enable: false,
           },
           number: {
             density: {
               enable: true,
               area: 800,
             },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
+            value: 0, // Start with no particles
           },
           size: {
-            value: { min: 1, max: 5 },
+            value: 5,
           },
         },
         detectRetina: true,

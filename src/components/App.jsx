2929d5d7521fc41bd181892e3d3fc226704d7eb3
Kodo <kodo2001k@gmail.com>
2024-03-27 00:01:09 +0300

    change px-2 to px-3 in our dev in app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,8 @@ import Questions from "./Questions";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5">
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-3 py-5">
       <Questions />
-      
     </div>
   );
 }

9f8c5f27381c858bbf861ae5b99c1609642401f9
Kodo <kodo2001k@gmail.com>
2024-03-27 02:39:35 +0300

    back to normal case

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Questions from "./Questions";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-3 py-5">
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5">
       <Questions />
     </div>
   );

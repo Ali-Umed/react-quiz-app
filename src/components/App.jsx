d43ee882bcc20701d06773a7641d35561408f623
Diyari Mohammed <diyarimohammed33@gmail.com>
2024-03-26 18:09:40 +0300

    Background style changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Questions from "./Questions";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5">
+    <div id="questions_container" className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5 overflow-hidden">
       <Questions />
     </div>
   );

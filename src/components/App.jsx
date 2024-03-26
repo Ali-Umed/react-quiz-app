71699898ce88c5e5146302e68890043bf65778cd
Kodo <kodo2001k@gmail.com>
2024-03-26 22:34:16 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5">
       <Questions />
+      
     </div>
   );
 }

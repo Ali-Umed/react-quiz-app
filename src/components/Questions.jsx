4201dcdfac63fda79f9a29507177da49a4cdd6cc
Diyari Mohammed <diyarimohammed33@gmail.com>
2024-03-26 04:49:03 +0300

    Questions and Questions Detail Component added

---
+++ src/components/Questions.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import QuestionsDetail from "./QuestionsDetail";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+import { BiAlarm } from "react-icons/bi";
+
+export default function Questions() {
+  return (
+    <section className="w-[35rem] flex flex-col gap-2 justify-center items-center py-5">
+      <header>
+        <h2 className="text-xl text-slate-400">Islamic Questions</h2>
+      </header>
+
+      <main className="p-2 bg-slate-800 rounded-md mt-5 max-w-[35rem]">
+        <QuestionsDetail />
+      </main>
+      <footer className="flex justify-between items-center w-full my-3">
+        <div className="flex justify-center items-center gap-1 rounded-md bg-slate-800 p-[0.65rem]">
+          <BiAlarm size={25} />
+          <p>1:00</p>
+        </div>
+        <div className="flex justify-center items-center gap-1 rounded-md bg-green-600 p-[0.65rem] hover:cursor-pointer hover:bg-green-500">
+          <button>Next Question</button>
+          <GoChevronRight size={25} />
+        </div>
+      </footer>
+    </section>
+  );
+}

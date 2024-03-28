f88bc92aabf83cd723f2fb975dddba144e300920
Kodo <kodo2001k@gmail.com>
2024-03-28 17:01:59 +0300

    add Coins component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
+import Coins from "./Coins";
 import Questions from "./Questions";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5">
-      <Questions />
+    <div className="flex">
+    <div className="w-3/4">
+      <div className="w-full h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5">
+        <Questions />
+      </div>
     </div>
+    <div className="w-1/4 bg-slate-800">
+      <Coins />
+    </div>
+  </div>
   );
 }
 

ec66c8412f18878e2dc2a0525fc15b705e132a24
Diyari Mohammed <diyarimohammed33@gmail.com>
2024-04-01 00:03:54 +0300

    First Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,10 @@ import Questions from "./Questions";
 
 function App() {
   return (
-    <div id="questions_container" className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5 overflow-hidden">
+    <div
+      id="questions_container"
+      className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white px-2 py-5 overflow-hidden"
+    >
       <Questions />
     </div>
   );

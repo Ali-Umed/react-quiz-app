902d29139fb327656ffb6130002ebc5193d02c8f
Ali-Umed <aliumed844@gmail.com>
2024-04-24 02:48:43 +0300

    feature: add loading status

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import Coins from "./Coins";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
@@ -14,8 +14,8 @@ const initState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case "dataRecevied":
-      return { ...state, questions: action.payload };
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
   }
 }
 
@@ -30,6 +30,13 @@ function App() {
     setIsDayMode(!isDayMode);
   };
 
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="flex">
       <div className="w-screen h-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 items-center bg-slate-900 text-white px-2 py-5">
@@ -39,7 +46,7 @@ function App() {
         <div className="col-span-3 place-self-center">
           {/* <SpinWeel /> */}
           {status == "loading" && <Loader />}
-          <Questions />
+          {status == "ready" && <Questions />}
         </div>
         <Footer />
         {/* <Coins /> */}
